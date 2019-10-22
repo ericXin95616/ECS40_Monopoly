12500d25c0a9059cadbfa9e345f3616c136cf5c4
ericXin95616 <xinbochao97@outlook.com>
2019-10-22 10:28:23 -0400

    Upload ECS40 Project: Monopoly

---
+++ cmake-build-debug/CMakeFiles/ECS40_Monopoly.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/bochao/CLionProjects/ECS40_Monopoly/Game.cpp" "/home/bochao/CLionProjects/ECS40_Monopoly/cmake-build-debug/CMakeFiles/ECS40_Monopoly.dir/Game.cpp.o"
+  "/home/bochao/CLionProjects/ECS40_Monopoly/OtherClass.cpp" "/home/bochao/CLionProjects/ECS40_Monopoly/cmake-build-debug/CMakeFiles/ECS40_Monopoly.dir/OtherClass.cpp.o"
+  "/home/bochao/CLionProjects/ECS40_Monopoly/Property.cpp" "/home/bochao/CLionProjects/ECS40_Monopoly/cmake-build-debug/CMakeFiles/ECS40_Monopoly.dir/Property.cpp.o"
+  "/home/bochao/CLionProjects/ECS40_Monopoly/main.cpp" "/home/bochao/CLionProjects/ECS40_Monopoly/cmake-build-debug/CMakeFiles/ECS40_Monopoly.dir/main.cpp.o"
+  "/home/bochao/CLionProjects/ECS40_Monopoly/player.cpp" "/home/bochao/CLionProjects/ECS40_Monopoly/cmake-build-debug/CMakeFiles/ECS40_Monopoly.dir/player.cpp.o"
+  "/home/bochao/CLionProjects/ECS40_Monopoly/rules.cpp" "/home/bochao/CLionProjects/ECS40_Monopoly/cmake-build-debug/CMakeFiles/ECS40_Monopoly.dir/rules.cpp.o"
+  "/home/bochao/CLionProjects/ECS40_Monopoly/setGame.cpp" "/home/bochao/CLionProjects/ECS40_Monopoly/cmake-build-debug/CMakeFiles/ECS40_Monopoly.dir/setGame.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ECS40_Monopoly.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ECS40_Monopoly.
+# This may be replaced when dependencies are built.
